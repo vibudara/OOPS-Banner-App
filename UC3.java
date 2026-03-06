504ae79d97061eb941a5a2f457f79470c613610f
vibudara <vibudara2507@gmail.com>
2026-03-06 21:46:56 +0530

    Print OOPS

---
+++ UC3.java
@@ -0,0 +1,33 @@
+public class UC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                " ******** ", " ******** ", " ******** ", " *******"
+        ));
+
+        System.out.println(String.join("",
+                "*        *", "*        *", "*        *", "*        "
+        ));
+
+        System.out.println(String.join("",
+                "*        *", "*        *", "*        *", "*        "
+        ));
+
+        System.out.println(String.join("",
+                "*        *", "*        *", " ******** ", " *******"
+        ));
+
+        System.out.println(String.join("",
+                "*        *", "*        *", "*        ", "        *"
+        ));
+
+        System.out.println(String.join("",
+                "*        *", "*        *", "*        ", "        *"
+        ));
+
+        System.out.println(String.join("",
+                " ******** ", " ******** ", "*        ", " ******** "
+        ));
+    }
+}
