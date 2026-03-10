de510b206a6976b17c1291478028442c70c0c0f7
vibudara <vibudara2507@gmail.com>
2026-03-10 16:59:34 +0530

    Coded to Print OOPS Banner in the console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,33 @@
 public class OOPSBannerApp {
-   public static void main(String[] args) {
-        
-      System.out.println("OOPS"); 
-      }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                " ******** " + " ******** " + " ******** " + " ******* "
+        );
+
+        System.out.println(
+                "*        *" + "*        *" + "*        *" + "*        "
+        );
+
+        System.out.println(
+                "*        *" + "*        *" + "*        *" + "*        "
+        );
+
+        System.out.println(
+                "*        *" + "*        *" + " ******** " + " ******* "
+        );
+
+        System.out.println(
+                "*        *" + "*        *" + "*        " + "        *"
+        );
+
+        System.out.println(
+                "*        *" + "*        *" + "*        " + "        *"
+        );
+
+        System.out.println(
+                " ******** " + " ******** " + "*        " + " ******** "
+        );
+    }
 }
