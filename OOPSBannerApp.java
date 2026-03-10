1783b8ea6cd3e75d5b676f203ae68efaa9ca3195
vibudara <vibudara2507@gmail.com>
2026-03-10 22:25:19 +0530

    Coded to Print OOPS Banner UC4 in the console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,32 +2,18 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join("",
-                " ******** ", " ******** ", " ******** ", " *******"
-        ));
+        String[] banner = new String[7];
 
-        System.out.println(String.join("",
-                "*        *", "*        *", "*        *", "*        "
-        ));
+        banner[0] = String.join("", " ******** ", " ******** ", " ******** ", " *******");
+        banner[1] = String.join("", "*        *", "*        *", "*        *", "*       ");
+        banner[2] = String.join("", "*        *", "*        *", "*        *", "*       ");
+        banner[3] = String.join("", "*        *", "*        *", " ******** ", " *******");
+        banner[4] = String.join("", "*        *", "*        *", "*        ", "        *");
+        banner[5] = String.join("", "*        *", "*        *", "*        ", "        *");
+        banner[6] = String.join("", " ******** ", " ******** ", "*        ", " ******** ");
 
-        System.out.println(String.join("",
-                "*        *", "*        *", "*        *", "*        "
-        ));
-
-        System.out.println(String.join("",
-                "*        *", "*        *", " ******** ", " *******"
-        ));
-
-        System.out.println(String.join("",
-                "*        *", "*        *", "*        ", "        *"
-        ));
-
-        System.out.println(String.join("",
-                "*        *", "*        *", "*        ", "        *"
-        ));
-
-        System.out.println(String.join("",
-                " ******** ", " ******** ", "*        ", " ******** "
-        ));
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
