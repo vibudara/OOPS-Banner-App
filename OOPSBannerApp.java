3f898178fe0d7c91a14727b04fff5b58e7fe106b
vibudara <vibudara2507@gmail.com>
2026-03-10 17:03:35 +0530

    Coded to Print OOPS Banner UC3 in the console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,32 +2,32 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(
-                " ******** " + " ******** " + " ******** " + " ******* "
-        );
+        System.out.println(String.join("",
+                " ******** ", " ******** ", " ******** ", " *******"
+        ));
 
-        System.out.println(
-                "*        *" + "*        *" + "*        *" + "*        "
-        );
+        System.out.println(String.join("",
+                "*        *", "*        *", "*        *", "*        "
+        ));
 
-        System.out.println(
-                "*        *" + "*        *" + "*        *" + "*        "
-        );
+        System.out.println(String.join("",
+                "*        *", "*        *", "*        *", "*        "
+        ));
 
-        System.out.println(
-                "*        *" + "*        *" + " ******** " + " ******* "
-        );
+        System.out.println(String.join("",
+                "*        *", "*        *", " ******** ", " *******"
+        ));
 
-        System.out.println(
-                "*        *" + "*        *" + "*        " + "        *"
-        );
+        System.out.println(String.join("",
+                "*        *", "*        *", "*        ", "        *"
+        ));
 
-        System.out.println(
-                "*        *" + "*        *" + "*        " + "        *"
-        );
+        System.out.println(String.join("",
+                "*        *", "*        *", "*        ", "        *"
+        ));
 
-        System.out.println(
-                " ******** " + " ******** " + "*        " + " ******** "
-        );
+        System.out.println(String.join("",
+                " ******** ", " ******** ", "*        ", " ******** "
+        ));
     }
 }
